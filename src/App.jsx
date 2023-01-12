f570bd2f3b206672f74679edc0ae6d1f1d879443
iuryteno23 <iurytenorio2005@gmail.com>
2023-01-12 20:32:52 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { InputFilter } from "./assets/multinput"
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <InputFilter />
+    </div>
+  )
+}
+
+export default App
